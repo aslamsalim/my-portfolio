05029f8a3b2c90dc45a7e6e5736de0b44caadfda
aslamsalim <aslamsalimnit@gmail.com>
2024-02-15 14:00:53 +0530

    intro 3

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,16 @@ export default function Home() {
       <button>click me</button>
       <p>
         In this portfolio, I invite you to explore a collection of projects that
-        embody my passion for creating captivating and functional spaces. Each
-        design is a testament to my dedication to innovation, sustainability,
-        and user-centricity. From residential gems to commercial landmarks,
-        these projects showcase my commitment to crafting environments that
-        transcend the ordinary. Thank you for joining me on this journey of
-        architectural exploration.
+        embody my passion for creating captivating and functional spaces.
+        <br>
+          Each design is a testament to my dedication to innovation,
+          sustainability, and user-centricity.
+        </br>{" "}
+        <br>
+          From residential gems to commercial landmarks, these projects showcase
+          my commitment to crafting environments that transcend the ordinary.
+        </br>{" "}
+        Thank you for joining me on this journey of architectural exploration.
       </p>
     </>
   );
