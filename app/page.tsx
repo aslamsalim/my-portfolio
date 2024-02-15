dd262384b98cf67613e919533d91a8e4bcd88eb2
aslamsalim <aslamsalimnit@gmail.com>
2024-02-15 14:03:10 +0530

    projects

--- app/page.tsx
+++ app/page.tsx
@@ -10,19 +10,7 @@ export default function Home() {
       <p>hello</p>
 
       <button>click me</button>
-      <p>
-        In this portfolio, I invite you to explore a collection of projects that
-        embody my passion for creating captivating and functional spaces.
-        <br>
-          Each design is a testament to my dedication to innovation,
-          sustainability, and user-centricity.
-        </br>{" "}
-        <br>
-          From residential gems to commercial landmarks, these projects showcase
-          my commitment to crafting environments that transcend the ordinary.
-        </br>{" "}
-        Thank you for joining me on this journey of architectural exploration.
-      </p>
+      <h2>My Projects</h2>
     </>
   );
 }
